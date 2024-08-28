4fd7719bd6e10bb3425ff23c013d08ade7b6cc97
Manpa Barman <manpabarmanaec@gmail.com>
2024-08-28 16:09:22 +0200

    Added dashboard config to create dashboard function

--- pipeline.py
+++ pipeline.py
@@ -1,28 +1,39 @@
 import pandas as pd
 import streamlit as st
+from src.source_data import source_all_data
+import altair as alt
+import src.streamlit_app.page_layout_config as page_layout_config
+from PIL import Image
 
-def source_historic_visitor_counting_data():
-    print("Historic Visitor Counting Data sourced.")
-
-def source_current_bayern_cloud_parking_data():
-    print("Current Bayern Cloud Parking Data sourced.")
-
-def process_historic_visitor_counting_data():
-    print("Historic Visitor Counting Data processed.")
-
-
-def pipeline():
-    source_historic_visitor_counting_data()
-    source_current_bayern_cloud_parking_data()
-    process_historic_visitor_counting_data()
 
 def create_dashboard():
-    st.title('Bavarian Forest - Digital Visitor Monitoring')
+    col1, col2 = page_layout_config.get_page_layout()
 
-    def visualize_current_bayern_cloud_parking_data():
-        print("Current Bayern Cloud Parking Data visualized.")
+    with col1:
+        logo = Image.open("src/streamlit_app/assets/bf_logo2.png")
+        st.image(logo, width=100)
+        st.title("Plan Your Trip to the Bavarian Forest")
 
+    with col2:
+        st.write("Hello col 2")
+        # # get the language selection menu
+        # get_language_selection_menu()
+        
+        # # get the weather section
+        # get_weather_section()
 
+        # # create recreational section
+        # get_recreation_section()
+
+    
+
+def pipeline():
+
+    historic_visitor_counts, all_parking_data, weather_data_df = source_all_data()
+
+    print(historic_visitor_counts.head(), all_parking_data.head(), weather_data_df.head())
+
+    return
 
 if __name__ == "__main__":
 
