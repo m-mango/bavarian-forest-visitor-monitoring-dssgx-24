49516ba850415a1e5a2133ebe16ef250d72dcc0c
Manpa Barman <manpabarmanaec@gmail.com>
2024-09-06 14:22:59 +0200

    added data accesibility section to the data accessibility dashboard

--- Pages/data_assessibility.py
+++ Pages/data_assessibility.py
@@ -3,6 +3,7 @@ import streamlit as st
 import altair as alt
 from src.streamlit_app.data_accessibility.query_box import get_query_section
 from src.streamlit_app.data_accessibility.query_viz_and_download import get_visualization_section
+from src.streamlit_app.data_accessibility.upload_download import get_upload_and_download_section
 
 # Define the page layout of the Streamlit app
 
@@ -17,6 +18,8 @@ alt.themes.enable("dark")
 # Define the app layout
 
 col1, col2 = st.columns((1.5,2), gap='medium')
+with col1:
+    get_upload_and_download_section()
 with col2:
     get_query_section()
 
