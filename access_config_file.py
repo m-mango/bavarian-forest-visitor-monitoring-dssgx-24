164e22fec5105a4ab1b15b9d0b68768405e13b13
Manpa Barman <manpabarmanaec@gmail.com>
2024-09-09 13:18:08 +0200

    removed the extra slash

--- access_config_file.py
+++ access_config_file.py
@@ -4,7 +4,7 @@ def get_aws_credentials():
 
     with open('config/aws_profile.toml', 'r') as f:
         config = toml.load(f)
-    \
+    
     # get the aws profile
     aws_profile = config['profile']['name']
     return aws_profile
