8387c120639d2e8c55d6b127c0ae95af179f5fff
Julia Ostheimer <42974945+JOPloume@users.noreply.github.com>
2024-10-15 13:10:57 +0200

    Define the start and end time for running inference more clearly and print this when inference fragment is run

--- Dashboard.py
+++ Dashboard.py
@@ -125,8 +125,11 @@ def run_inference(preprocessed_hourly_visitor_center_data):
         
         return day_today_berlin
 
-    weather_data_inference = source_weather_data(start_time = get_today_midnight_berlin() - pd.Timedelta(days=10), 
-                                                 end_time = get_today_midnight_berlin() + pd.Timedelta(days=7))
+    start_inference_time = get_today_midnight_berlin() - pd.Timedelta(days=10)
+    end_inference_time = get_today_midnight_berlin() + pd.Timedelta(days=7)
+    print(f"Running inference part from {start_inference_time} to {end_inference_time}...")
+
+    weather_data_inference = source_weather_data(start_time=start_inference_time, end_time=end_inference_time)
 
     # preprocess the inference data
     inference_df = source_preprocess_inference_data(weather_data_inference, preprocessed_hourly_visitor_center_data)
